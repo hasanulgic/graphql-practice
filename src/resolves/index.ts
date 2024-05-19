335321127fd6d884ab9db05d800f706c7873898a
hasanulgic <hasanulgic@gmail.com>
2024-05-19 12:34:14 +0600

    signup done

--- src/resolves/index.ts
+++ src/resolves/index.ts
@@ -1,16 +1,33 @@
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-];
+import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+interface userInfo {
+  name: string;
+  email: string;
+  password: string;
+}
 
 export const resolvers = {
   Query: {
-    books: () => books,
+    users: async (parent: any, args: any, context: any) => {
+      return await prisma.user.findMany();
+    },
+  },
+  Mutation: {
+    signup: async (parent: any, args: userInfo, context: any) => {
+      console.log(args);
+      const hashed = await bcrypt.hash(args.password, 12);
+      const newUser = await prisma.user.create({
+        data: {
+          ...args,
+          password: hashed,
+        },
+      });
+
+      const token = jwt.sign({userId: newUser.id}, "signature", {expiresIn: "1d"})
+      console.log(token)
+    },
   },
 };
--- src/schema.ts
+++ src/schema.ts
@@ -1,11 +1,40 @@
 export const typeDefs = `#graphql
 
-  type Book {
-    title: String
-    author: String
-  }
-  
   type Query {
-    books: [Book]
+    me: User
+    users: [User]
+    posts: [Post]
   }
+
+  type Mutation {
+    signup(
+      name: String!,
+      email: String!,
+      password: String!
+    ): User
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    content: String!
+    author: User
+    createdAt: String!
+    published: Boolean!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    createdAt: String!
+    posts: [Post]
+  }
+
+  type Profile {
+    id: ID!
+    bio: String!
+    createdAt: String!
+  }
+ 
 `;
