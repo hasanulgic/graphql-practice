01ac35c1dfcc62baf9e5ee928efb36ab9d672a09
hasanulgic <hasanulgic@gmail.com>
2024-05-29 12:07:27 +0600

    authentication and validation done

--- src/resolves/index.ts
+++ src/resolves/index.ts
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
@@ -17,7 +17,19 @@ export const resolvers = {
   },
   Mutation: {
     signup: async (parent: any, args: userInfo, context: any) => {
-      console.log(args);
+      const isExist = await prisma.user.findFirst({
+        where: {
+          email: args.email,
+        },
+      });
+
+      if(isExist){
+        return {
+          userError: "Already this email is registered",
+          token: null
+        }
+      }
+
       const hashed = await bcrypt.hash(args.password, 12);
       const newUser = await prisma.user.create({
         data: {
@@ -26,8 +38,42 @@ export const resolvers = {
         },
       });
 
-      const token = jwt.sign({userId: newUser.id}, "signature", {expiresIn: "1d"})
-      console.log(token)
+      const token = jwt.sign({ userId: newUser.id }, "signature", {
+        expiresIn: "1d",
+      });
+      return {
+        userError: null,
+        token,
+      };
+    },
+    signin: async (parent: any, args: any, context: any) => {
+      // console.log(args);
+      const user = await prisma.user.findFirst({
+        where: {
+          email: args.email,
+        },
+      });
+      // console.log(user);
+      if (!user) {
+        return {
+          userError: "User not found",
+          token: null,
+        };
+      }
+      const correctPass = await bcrypt.compare(args.password, user.password);
+      if (!correctPass) {
+        return {
+          userError: "Wrong Password provided",
+          token: null,
+        };
+      }
+      const token = jwt.sign({ userId: user.id }, "signature", {
+        expiresIn: "1d",
+      });
+      return {
+        userError: null,
+        token,
+      };
     },
   },
 };
