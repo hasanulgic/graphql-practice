6a99561bca65d3f7626bc356622a24c77f8af1b8
hasanulgic <hasanulgic@gmail.com>
2024-06-01 19:10:36 +0600

    populated data fetch done

---
+++ src/reslovers/profile.ts
@@ -0,0 +1,10 @@
+export const Profile = {
+  user: async (parent: any, args: any, { prisma, userInfo }: any) => {
+    return await prisma.user.findUnique({
+      where: {
+        id: parent.userId,
+      },
+    });
+  },
+};
+ 
